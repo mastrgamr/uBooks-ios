e9f1ccd123c2a90393c5be3da2fbb1b5f69a3839
mastrgamr <themastrgamr@gmail.com>
2016-04-09 18:13:49 -0400

    Update framworks

---
+++ lastminute/DBManager.swift
@@ -0,0 +1,15 @@
+//
+//  DBManager.swift
+//  lastminute
+//
+//  Created by stuart on 4/9/16.
+//  Copyright © 2016 lastminute. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
+
+class DBManager : NSObject {
+    
+}
